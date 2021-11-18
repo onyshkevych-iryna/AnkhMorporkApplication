34fb9652a3b1cebf529e5614e8cdecba55fe0b4d
onyshkevych-iryna <iryna.onyshkevych@valtech.com>
2021-11-18 12:03:43 +0200

    Initial commit

---
+++ AnkhMorporkApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AnkhMorporkApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
